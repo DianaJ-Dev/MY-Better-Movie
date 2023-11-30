ce3af5d3bef0f9320a06de4e2afca93eac02f467
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-11-30 17:02:06 -0500

    se traen peliculas por cada peticion de pagina

--- src/components/movies.tsx
+++ src/components/movies.tsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from "react";
-import React from 'react'
-import { ShowPaginated } from "./showPaginated";
-
+import React, { useState, useEffect } from "react"
+import { ShowPaginated } from "./showPaginated"
+import styles from './movies.module.css';
 
 // Interfaz TypeScript que describe la estructura de objetos
 interface Movie {
@@ -11,26 +10,40 @@ interface Movie {
 
 }
 
+const includeImage = 'https://image.tmdb.org/t/p/w200/'
+
 export const Movies = () => {
-  // TRAER PELICULAS API
-  const [movies, setMovies] = useState<Movie[]>([]); // Actualiza el valor de estado Movie (desestructuración)
-  const initialUrl = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=Action%2C%20Adventure%2CComedyAnimation%2CDrama%2C%20Science%20Fiction&api_key=a96958b664d1a603a39c9d2064867790';
+  const [movies, setMovies] = useState<Movie[]>([]); // Actualiza el valor de estado de las peliculas  (desestructuración)
+  const [currentPage, setCurrentPage] = useState(1); // Actualiza el valor de las paginas 
 
+  const movieFetch = (page: number) => {
+    const initialUrl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&with_genres=Action%2C%20Adventure%2CComedyAnimation%2CDrama%2C%20Science%20Fiction&api_key=a96958b664d1a603a39c9d2064867790`;
+
+    fetch(initialUrl)
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results))
+      .catch((err) => console.error(err));
+  };
+
+  // Nueva solicitud cada  vez que cambia la pelicula
   useEffect(() => {
-    const fetchMovies = async () => {
-      const promises = [];
-      for (let page = 1; page <= 100; page++) {
-        const url = `${initialUrl}&page=${page}`
-        promises.push(fetch(url).then(response => response.json()))
-      }
-      const results = await Promise.all(promises)
-      const allMovies = results.flatMap(result => result.results) // results informacion de cada pelicula 
-      setMovies(allMovies);
-    };
+    movieFetch(currentPage);
+  }, [currentPage]);
 
-    fetchMovies();
-  }, [initialUrl])
-
-  return <ShowPaginated movies={movies} />
-  
+  return (
+    <div className={styles.movies} >
+      {movies.map((item, index) => (
+        <header className={styles.card} key={index}>
+          <h4 className={styles.tittle}>{item.title}</h4>
+          <img
+            className={styles.img}
+            src={includeImage + item.poster_path}
+            alt={item.title}
+          />
+          <h4 className={styles.tittle}>{item.release_date}</h4>
+        </header>
+      ))}
+      <ShowPaginated movies={movies} setCurrentPage={setCurrentPage} selected={currentPage}/>
+    </div>
+  )
 }
